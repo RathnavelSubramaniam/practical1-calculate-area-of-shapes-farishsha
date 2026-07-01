5717706a983c1bbe15e6b0568bbc2241198bda0c
farishbca <farishsha15@gmail.com>
2026-07-01 08:10:58 +0000

    update

--- Practical1.java
+++ Practical1.java
@@ -38,46 +38,46 @@ public Trapezoid(Point p1, Point p2, Point p3, Point p4) {
 super(p1, p2, p3, p4);
 }
 public double calculateArea(){
-    double base1 = Math.abs(point1.getX()-point2.getX());
-    double base2 = Math.abs(point3.getX()-point4.getX());
-    double height =Math.abs(point1.getY()-point3.getY());
-    return (base1+base2)*height/2.0;
+    double base1 =Math.abs(point1.getX()-point2.getX());
+    double base2 =Math.abs(point3.getX()-point4.getX());
+    double height=Math.abs(point1.getY()-point3.getY());
+    return(base1+base2)* height/2.0;
 }
 }
 class Rectangle extends Quadrilateral{
-    public Rectangle(Point p1,Point p2,Point p3,Point p4){
+    public Rectangle(Point p1,Point p2,Point p3,Point p4)
+    {
         super(p1,p2,p3,p4);
     }
     public double calculateArea(){
-        double length = Math.abs(point1.getX()-point2.getX());
-        double width = Math.abs(point2.getY()-point3.getY());
-        return length*width;
+        double length =Math.abs(point1.getX()-point2.getX());
+        double width=Math.abs(point2.getY()-point4.getY());
+        return length * width;
     }
 }
 class Square extends Rectangle{
-    double sideLength;
-    public Square(Point p1,double sideLength)
-    {
-        super(p1,new Point(p1.getX()+sideLength,p1.getY()),
+    public Square(Point p1,double sideLength){
+        super(p1, new Point(p1.getX()+sideLength,p1.getY()),
         new Point(p1.getX()+sideLength,p1.getY()+sideLength),
         new Point(p1.getX(),p1.getY()+sideLength));
-        this.sideLength = sideLength;
     }
     public double calculateArea(){
-        return sideLength*sideLength;
+        double sideLength = Math.abs(point1.getX()-point2.getX());
+        return sideLength * sideLength;
     }
-    class program1{
-        public static void main(String[]args){
+}
+    class Practical1{
+        public static void main(String[] args){
             Point p1 = new Point(0,0);
             Point p2 = new Point(4,0);
             Point p3 = new Point(4,3);
             Point p4 = new Point(0,3);
-            Trapezoid trapezoid = new Trapezoid(p1,p2,p3,p4);
-            System.out.println("Area of Trapezoid: "+trapezoid.calculateArea());
+            Trapezoid trapezoid= new Trapezoid(p1,p2,p3,p4);
             Rectangle rectangle = new Rectangle(p1,p2,p3,p4);
-            System.out.println("Area of Rectangle: "+rectangle.calculateArea());
             Square square = new Square(p1,3);
-            System.out.println("Area of Square: "+square.calculateArea());
+            System.out.println("Area of trapezoid="+trapezoid.calculateArea());
+            System.out.println("Area of rectangle="+rectangle.calculateArea());
+            System.out.println("Area of square="+square.calculateArea());
+
         }
     }
-}
